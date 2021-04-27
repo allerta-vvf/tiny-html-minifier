e5850353dece11a1971d26126aa87780991ce826
Matteo Gheza <matteo.gheza07@gmail.com>
2021-04-28 00:18:33 +0200

    Update TinyHtmlMinifier.php

--- src/TinyHtmlMinifier.php
+++ src/TinyHtmlMinifier.php
@@ -79,13 +79,13 @@ class TinyHtmlMinifier
             $rest = (isset($parts[1])) ? $parts[1] : '';
         }
 
-        return $this->output;
+        return mb_convert_encoding($this->output, "UTF-8");;
     }
 
-    // Walk trough html
+    // Walk through html
     private function walk(&$part)
     {
-        $tag_parts = explode('>', $part);
+        $tag_parts = explode('>', $part, 2);  // Limit of 2 parts to prevent issues with '>' in content (eg. JS comparison)
         $tag_content = $tag_parts[0];
 
         if (!empty($tag_content)) {
